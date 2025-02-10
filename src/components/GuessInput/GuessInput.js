f80b25b7ce89f019b73a6a8c6c948d840eb2fd01
Erlend Geerts <erlend.geerts@gmail.com>
2025-02-10 08:35:25 +0100

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (guess.length !== 5) {
+      alert("Please enter a 5 letter word");
+      return;
+    }
+    console.log({ guess });
+    setGuess("");
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        minLength={5}
+        maxLength={5}
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
